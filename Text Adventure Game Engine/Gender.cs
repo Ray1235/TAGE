c9bb65b32bbda78c419042e3035d55491a172b2b
Ray1235 <ray1235@me.com>
2016-06-30 00:21:14 +0200

    Added info for every file

--- Text Adventure Game Engine/Gender.cs
+++ Text Adventure Game Engine/Gender.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+===========================================================================
+
+TAGE GPL Source Code
+Copyright (C) 2016 Ray1235
+
+This file is part of the TAGE GPL Source Code.  
+
+TAGE Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TAGE Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TAGE Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
+
+/* File:        Gender.cs
+ * Purpose:     Unused class for gender
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +37,9 @@ namespace Text_Adventure_Game_Engine
 {
     class Gender
     {
-        public Type Male;
-        public Type Female;
+        public enum _n {
+            Male,
+            Female
+        }
     }
 }
--- Text Adventure Game Engine/Models/Vert.cs
+++ Text Adventure Game Engine/Models/Vert.cs
@@ -1,4 +1,32 @@
-﻿using System;
+﻿/*
+===========================================================================
+
+TAGE GPL Source Code
+Copyright (C) 2016 Ray1235
+
+This file is part of the TAGE GPL Source Code.  
+
+TAGE Source Code is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TAGE Source Code is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TAGE Source Code.  If not, see <http://www.gnu.org/licenses/>.
+
+===========================================================================
+*/
+
+/* File:        Vert.cs
+ * Purpose:     Class for storing info for a single vertex
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
